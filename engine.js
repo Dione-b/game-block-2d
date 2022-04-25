42dc8419d7d6dc6960316f67f96a8f5e950a3b33
dione-b <dionenassau321@gmail.com>
2022-04-25 16:53:54 -0300

    second commit

--- engine.js
+++ engine.js
@@ -1,6 +1,210 @@
 // Captura o id do canva
-var canvas = document.getElementById("myCanvas")
+var canvas = document.getElementById("myCanvas");
+var text = document.getElementById("text");
 
 // Responsável por gerar as pinturas no canva
-var ctx = canvas.getContext("2d")
+var ctx = canvas.getContext("2d");
 
+// Contém o raio do círculo desenhado
+var ballRadius = 10;
+
+// Captura dados de altura/largura do canva e suas dimenções x e y
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
+// Criando dimenções da raquete para rebater a bola
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+// Direções da raquete
+var rightPressed = false;
+var leftPressed = false;
+
+// Criando as dimensões tijolos 
+var brickRowCount = 5;
+var brickColumnCount = 3;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 50;
+var brickOffsetLeft = 55;
+
+// Criando score
+var score = 0;
+// Vidas 
+var lives = 1;
+
+// Construindo a posição dos tijolos
+var bricks = [];
+
+for(var c=0; c<brickColumnCount; c++) {
+  bricks[c] = [];
+  for(var r=0; r<brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+// Criando evento de tecla de botão
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if(relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth/2;
+  }
+}
+
+// Função de colisão com os tijolos
+function collisionDetection() {
+  for(var c=0; c<brickColumnCount; c++) {
+    for(var r=0; r<brickRowCount; r++) {
+      var b = bricks[c][r];
+      if(b.status == 1) {
+        if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+          if(score == brickRowCount*brickColumnCount) {
+            text.innerHTML = "ACERTOU!!!" // adicionado campo para informar o usuário que ele perdeu
+            text.style.color = "blue";
+            canvas.onanimationcancel(); // funcão que para toda a animação
+          }
+        }
+      }
+    }
+  }
+}
+
+// Função para desenhar a bola
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Função para desenhar a raquete
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Função para desenhar os tijolos
+function drawBricks() {
+  for(var c=0; c<brickColumnCount; c++) {
+    for(var r=0; r<brickRowCount; r++) {
+      if(bricks[c][r].status == 1) {
+        var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+        var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
+// Função para desenhar o score
+function drawScore() {
+  ctx.font = "14px Gameplay";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: "+score, 8, 20);
+}
+
+// Função para desenhar a vida
+function drawLives() {
+  ctx.font = "14px Gameplay";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+}
+
+// Função que movimenta e limpa os rastros da bola
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawLives();
+  collisionDetection();
+
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if(y + dy < ballRadius) {
+    dy = -dy;
+  }
+  else if(y + dy > canvas.height-ballRadius) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    }
+    else {
+      lives--;
+      if(!lives) {
+        text.innerHTML = "GAMER OVER"; // adicionado campo para informar o usuário que ele perdeu
+        canvas.onanimationcancel(); // funcão que para toda a animação
+      }
+      else {
+        x = canvas.width/2;
+        y = canvas.height-30;
+        dx = 3;
+        dy = -3;
+        paddleX = (canvas.width-paddleWidth)/2;
+      }
+    }
+  }
+  // Verifica se as teclas de cursor esquerda ou direita são pressionadas,
+  // quando cada quadro é renderizado
+  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+  }
+  else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
+  x += dx;
+  y += dy;
+  requestAnimationFrame(draw);
+}
+
+// Funcão para animar texto
+var interval = window.setInterval(function(){
+    if(text.style.visibility == 'hidden'){
+        text.style.visibility = 'visible';
+    }else{
+        text.style.visibility = 'hidden';
+    }
+}, 850);
+
+draw();
